30f5edf655a2e4302d1e2588c6368df970d9866e
kumaraditya1999 <kumaraditya1999@gmail.com>
2018-12-31 19:32:35 +0530

    styling

--- assets/js/todoapp.js
+++ assets/js/todoapp.js
@@ -1,3 +1,37 @@
 $(document).ready(function(){
 
+
+
+	$('#login-modal').click(function(){
+		$('#registerModal').modal("hide");
+		$('#loginModal').modal("show");
+	});
+
+	$('#register-modal').click(function(){
+		$('#loginModal').modal("hide");
+		$('#registerModal').modal("show");
+	});
+
+	$('#login-link').click(function(){
+		$('#registerModal').modal("hide");
+		$('#loginModal').modal("show");
+	});
+
+	$('#register-link').click(function(){
+		$('#loginModal').modal("hide");
+		$('#registerModal').modal("show");
+	});
+	$('#login-link').css({"cursor" : "pointer"});
+	$('#register-link').css({"cursor" : "pointer"});
+
+
+	$('#login-btn').click(function(e){
+		e.preventDeafault();
+		console.log($('#first-name').val());
+	});
+
+	$('#register-btn').click(function(e){
+		e.preventDeafault();
+		console.log($('#first-name').val());
+	});
 });
